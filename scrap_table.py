fd6a5211597daa3cef4a5c3795f24f933e8437ea
Ambrose <arunsanthosh004@gmail.com>
2026-01-08 16:32:31 +0530

    For scrapping Tables in the website
    This code will be very useful when the website/site is dynamic, because sometimes table rows are injected dynamically via JavaScript, or other methods so using this method will be effective 
    
    **To get the xhr request URL**
    1.go the table site 
    2.give inspect
    3.move to network tab (give a refresh)
    4.under the type search for xhr the click it and Request URL will be displayed copy that and paste in the code

---
+++ scrap_table.py
@@ -0,0 +1,24 @@
+import requests
+import json
+
+url = "  " #your xhr type request URL here
+headers = {
+    "User-Agent": "Mozilla/5.0",
+    "X-Requested-With": "XMLHttpRequest"
+}
+
+response = requests.get(url, headers=headers)
+data = response.json()
+
+with open("table1.txt", "w", encoding="utf-8") as f: #you can give your preferable name (yourtextname.txt)
+    for item in data:
+        try:
+            name = json.loads(item.get("title", "{}")).get("en", "N/A")
+            role = json.loads(item.get("content", "{}")).get("en", "N/A")  # fallback to 'content' if 'or' is missing
+            phone = json.loads(item.get("number", "{}")).get("en", "N/A")
+            email = json.loads(item.get("excerpt", "{}")).get("en", "N/A")
+
+            line = f"{name} | {role} | {phone} | {email}\n" 
+            f.write(line) # write each row to the file
+        except Exception as e:
+            f.write(f"Error parsing item: {e}\n")
